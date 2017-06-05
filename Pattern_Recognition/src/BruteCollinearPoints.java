d5120eb5c5d841773acbe1720078c45bc26e0bd5
Chand, Vishal <vish-chan@users.noreply.github.com>
2017-06-05 18:42:55 +0530

    Second commit

--- Pattern_Recognition/src/BruteCollinearPoints.java
+++ Pattern_Recognition/src/BruteCollinearPoints.java
@@ -1,43 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
-    LineSegment[] mLinesegments;
+    private ArrayList<LineSegment> mLinesegments;
+    private ArrayList<Point> collinearMin, collinearMax;
 
     public BruteCollinearPoints(Point[] points) {
         // TODO Auto-generated constructor stub
         check(points);
-        this.mLinesegments = new LineSegment[points.length];
-        double slope_1, slope_2, slope_3;
-        int idx = 0;
-        for (int i = 0; i < points.length; i++)
-            for (int j = i + 1; j < points.length; j++)
-                for (int k = j + 1; k < points.length; k++) {
-                    slope_1 = points[i].slopeTo(points[j]);
-                    slope_2 = points[i].slopeTo(points[k]);
-                    if (slope_1 != slope_2)
+        this.mLinesegments = new ArrayList<>();
+        this.collinearMin = new ArrayList<>();
+        this.collinearMax = new ArrayList<>();
+        for (int i = 0; i < points.length - 3; i++) {
+            Point p1 = points[i];
+            for (int j = i + 1; j < points.length - 2; j++) {
+                Point p2 = points[j];
+                double slope12 = p1.slopeTo(p2);
+                for (int k = j + 1; k < points.length - 1; k++) {
+                    Point p3 = points[k];
+                    double slope13 = p1.slopeTo(p3);
+                    if (slope12 != slope13)
                         continue;
                     for (int l = k + 1; l < points.length; l++) {
-                        slope_3 = points[i].slopeTo(points[l]);
-                        if (slope_3 == slope_1)
-                            mLinesegments[idx++] = new LineSegment(points[i], points[l]);
+                        Point p4 = points[l];
+                        double slope14 = p1.slopeTo(p4);
+                        if (slope14 == slope12) {
+                            addLine(p1, p2, p3, p4);
+                        }
                     }
                 }
+            }
+        }
+        for (int i = 0; i < collinearMin.size(); i++)
+            mLinesegments.add(new LineSegment(collinearMin.get(i), collinearMax.get(i)));
+        collinearMin.clear();
+        collinearMax.clear();
+    }
+
+    private void addLine(Point p1, Point p2, Point p3, Point p4) {
+        Point[] lines = new Point[4];
+        lines[0] = p1;
+        lines[1] = p2;
+        lines[2] = p3;
+        lines[3] = p4;
+        Arrays.sort(lines);
+        Point min = lines[0];
+        Point max = lines[3];
+        for (int i = 0; i < collinearMin.size(); i++)
+            if (collinearMin.get(i) == min && collinearMax.get(i) == max)
+                return;
+        collinearMin.add(min);
+        collinearMax.add(max);
     }
 
     private void check(Point[] points) {
         if (points == null)
             throw new java.lang.NullPointerException();
-        for (int i = 0; i < points.length; i++)
-            if (points[i] == null)
-                throw new java.lang.NullPointerException();
+        for (int i = 0; i < points.length - 1; i++)
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i] == null || points[j] == null)
+                    throw new java.lang.NullPointerException();
+                if (points[i].compareTo(points[j])==0)
+                    throw new java.lang.IllegalArgumentException();
+            }
     }
 
     public int numberOfSegments() {
-        return mLinesegments.length;
+        return mLinesegments.size();
     }
 
     public LineSegment[] segments() {
-        return this.mLinesegments;
+        return mLinesegments.toArray(new LineSegment[mLinesegments.size()]);
     }
 
 }
--- Pattern_Recognition/src/FastCollinearPoints.java
+++ Pattern_Recognition/src/FastCollinearPoints.java
@@ -1,45 +1,96 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class FastCollinearPoints {
-    LineSegment[] mLinesegments;
+    private ArrayList<LineSegment> mLinesegments;
+    private ArrayList<Point> collinearMin, collinearMax;
 
     public FastCollinearPoints(Point[] points) {
         // TODO Auto-generated constructor stub
         check(points);
-        LineSegment[] mLinesegments = new LineSegment[points.length];
+        collinearMin = new ArrayList<>();
+        collinearMax = new ArrayList<>();
         for (int i = 0; i < points.length; i++) {
             Point p = points[i];
             Arrays.sort(points, p.slopeOrder());
             this.checkForCollinears(points, p);
         }
+        mLinesegments = new ArrayList<>();
+        for (int i = 0; i < collinearMin.size(); i++)
+            mLinesegments.add(new LineSegment(collinearMin.get(i), collinearMax.get(i)));
+        collinearMin.clear();
+        collinearMax.clear();
     }
 
     private void checkForCollinears(Point[] points, Point p) {
-        Double[] slopes = new Double[points.length];
+        double[] slopes = new double[points.length];
         for (int i = 0; i < points.length; i++) {
             slopes[i] = p.slopeTo(points[i]);
-        } 
-        int idx = 0;
+        }
+        assert isSorted(slopes);
         for (int i = 0; i < slopes.length;) {
             double cur = slopes[i];
-            int  j = i+1;
-            int count = 0;
-            while (slopes[j++] == cur) {
+            int j = i + 1;
+            if (j >= slopes.length)
+                break;
+            Point maxPoint = points[i];
+            Point minPoint = points[i];
+            int count = 1;
+            while (j < slopes.length && slopes[j] == cur) {
                 count++;
+                if (maxPoint.compareTo(points[j]) < 0)
+                    maxPoint = points[j];
+                if (minPoint.compareTo(points[j]) > 0)
+                    minPoint = points[j];
+                j++;
             }
-            if (count >= 3)
-                mLinesegments[idx++] = new LineSegment(p, points[j-1]);
+            if (count >= 3) {
+                if (maxPoint.compareTo(p) < 0)
+                    maxPoint = p;
+                if (minPoint.compareTo(p) > 0)
+                    minPoint = p;
+                if (!checkDup(minPoint, maxPoint)) {
+                    collinearMin.add(minPoint);
+                    collinearMax.add(maxPoint);
+                }
+            }
             i = j;
         }
+    }
+
+    private boolean checkDup(Point p1, Point p2) {
+        for (int i = 0; i < collinearMin.size(); i++)
+            if (collinearMin.get(i) == p1 && collinearMax.get(i) == p2)
+                return true;
+        return false;
+    }
+
+    private boolean isSorted(double[] a) {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < a.length - 1; i++)
+            if (a[i] > a[i + 1])
+                return false;
+        return true;
     }
 
     private void check(Point[] points) {
         if (points == null)
             throw new java.lang.NullPointerException();
-        for (int i = 0; i < points.length; i++)
-            if (points[i] == null)
-                throw new java.lang.NullPointerException();
+        for (int i = 0; i < points.length - 1; i++)
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i] == null || points[j] == null)
+                    throw new java.lang.NullPointerException();
+                if (points[i].compareTo(points[j])==0)
+                    throw new java.lang.IllegalArgumentException();
+            }
+    }
+
+    public int numberOfSegments() {
+        return mLinesegments.size();
+    }
+
+    public LineSegment[] segments() {
+        return mLinesegments.toArray(new LineSegment[mLinesegments.size()]);
     }
 
 }
